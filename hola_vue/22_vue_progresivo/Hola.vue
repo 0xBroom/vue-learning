20e37cf390b0ca7aea1ba6afd3359db958b11064
0xBroom <juantaban@gmail.com>
2023-09-17 11:22:02 +0200

    feat: add 22th lesson

---
+++ hola_vue/22_vue_progresivo/Hola.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <p>Lorem ipsum dolor</p>
+    <div>{{ text }}</div>
+  </div>
+</template>
+ 
+<script>
+export default {
+  data() {
+    return {
+      text: 'Hello, world!'
+    }
+  }
+}
+</script>
+ 
+<style>
+p {
+  color: red;
+}
+</style>
